02d72b9fb883b0e5e0e3119071c39209ef2c2be1
강병우 <peace0096@naver.com>
2024-01-11 00:24:14 +0900

    [Gold IV] Title: 줄세우기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.cc
@@ -0,0 +1,41 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <string>
+#include <queue>
+#include <map>
+#include <cstring>
+#include <sstream>
+using namespace std;
+
+int arr[201];
+int dp[201];
+
+int main()
+{
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		int x;
+		cin >> x;
+		arr[i] = x;
+	}
+	
+	int _max = 0;	// 최장 부분증가 수열 길이
+	for (int i = 1; i <= N; i++)
+	{
+		dp[i] = 1;
+		for (int j = 1; j < i; j++)
+		{
+			if (arr[i] > arr[j] && dp[i] <= dp[j])
+				dp[i] += 1;
+		}
+		_max = max(_max, dp[i]);
+	}
+	cout << N - _max << '\n';
+
+
+	return 0;
+}
