7b4f9c035a8c86cccfa8ee2da61b2397ec1af27e
강병우 <peace0096@naver.com>
2024-08-16 14:26:04 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,58 +1,38 @@
-#include <vector>
 #include <iostream>
-#include <algorithm>
-#include <stack>
-#include <string>
-#include <queue>
-#include <cstring>
+#include <vector>
+
 using namespace std;
-
-int cached[1000001];
-
-int recursive(int n)
-{
-	if (n == 1)
-		return 0;
-
-	int& ret = cached[n];
-
-	if (ret != -1)
-	{
-		return ret;
-	}
-
-	if (n % 6 == 0)
-	{
-		int per3 = recursive(n / 3);
-		int per2 = recursive(n / 2);
-		int minus = recursive(n - 1);
-		if (per3 >= per2)
-			return ret = min(per2, minus) + 1;
-		else if (per3 >= minus)
-			return ret = min(minus, per2) + 1;
-		else
-			return ret = min(per2, per3) + 1;
-	}
-	else if (n % 3 == 0)
-	{
-		return ret = min(recursive(n / 3), recursive(n - 1)) + 1;
-	}
-	else if (n % 2 == 0)
-	{
-		return ret = min(recursive(n / 2), recursive(n - 1)) + 1;
-	}
-	else
-	{
-		return ret = recursive(n - 1) + 1;
-	}
-}
+int N;
+int dp[1000001] = { };
 
 int main()
 {
-	int n;
-	memset(cached, -1, sizeof(cached));
-	cin >> n;
-	cout << recursive(n);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	cin >> N;
+	dp[1] = 0;
+	dp[2] = 1;
+	dp[3] = 1;
+
+
+	for (int i = 4; i <= N; i++)
+	{
+
+		dp[i] = 1000000;
+		if (i % 3 == 0)
+		{
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+		}
+		if (i % 2 == 0)
+		{
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		}
+		dp[i] = min(dp[i], dp[i - 1] + 1);
+		
+	}
+	
+	cout << dp[N];
+	
 	return 0;
 }
