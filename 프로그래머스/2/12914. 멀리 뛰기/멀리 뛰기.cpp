5dc9f8747cb079885bcd6134105849f9eb7ffec8
강병우 <peace0096@naver.com>
2025-03-03 18:36:21 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long dp[2001];
+
+long long solution(int n) {
+    long long answer = 0;
+    
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(int i = 3; i <= n; i++)
+    {
+        dp[i] = (dp[i - 1] % 1234567 + dp[i - 2] % 1234567) % 1234567;
+    }
+    answer = dp[n];
+    return answer;
+}
